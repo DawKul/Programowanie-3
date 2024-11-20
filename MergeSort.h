6a5a2aa052fb2ec9f4b0dce7a7e83070ac70e505
DawKul <dawidkgit@gmail.com>
2024-11-20 20:46:35 +0100

    Create MergeSort.h

---
+++ MergeSort.h
@@ -0,0 +1,14 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+#include <vector>
+
+class MergeSort {
+public:
+    static void sort(std::vector<int>& arr);
+private:
+    static void mergeSort(std::vector<int>& arr, int left, int right);
+    static void merge(std::vector<int>& arr, int left, int mid, int right);
+};
+
+#endif
